26f56fa551d37369fc42ae77627bbe0a9814bee3
Sabal <sigdelsabal20@gmail.com>
2026-06-27 12:51:21 +0545

    Update logic.js

---
+++ main/logic.js
@@ -0,0 +1,25 @@
+
+import {startBoard} from "./data.js";
+const flatBoard = startBoard.flat();
+
+
+function possibleSpaces(current,row, col){
+    const possible = [];
+
+   for(let i=row;i>=0;i++){
+    
+
+
+   }
+
+
+
+    if(current.closed){
+
+    }
+    
+
+
+
+
+}
