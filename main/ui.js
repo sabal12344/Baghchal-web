5436e09526d96447e85160560c36f3c5943f95d9
Sabal <sigdelsabal20@gmail.com>
2026-06-29 16:38:32 +0545

    Update ui.js

--- main/ui.js
+++ main/ui.js
@@ -1,6 +1,11 @@
 
-import {startBoard} from "./data.js";
-const flatboard = startBoard().flat();
+import {startBoard,tigerTurn,switchTurn,goatleft,piece, decreGoatleft} from "./data.js";
+import{possibleSpaces,makeTigerMove} from "./logic.js";
+const flatboard = startBoard.flat();
+let firstclick = true;
+
+ let sourceSpace;
+
 
 const board = document.getElementById('board1');
 board.addEventListener('click',(e)=>{
@@ -12,6 +17,7 @@ board.addEventListener('click',(e)=>{
 for(let i=0;i<25;i++){
     const space = document.createElement('div');
     space.className='space';
+    space.id = i;
     board.appendChild(space);
     
     const content = flatboard[i].piece;
@@ -31,6 +37,7 @@ function clearHighlight(){
     const all = document.getElementsByClassName('space');
      for(let i of all){
         i.style.border="none";
+        i.style.background="black";
     }
 }
 
@@ -39,12 +46,65 @@ function spaceClicked(space){
 
    clearHighlight();
    
-    if(space.innerHTML !=''){
-        space.style.border='7px solid black';
-        return;
-
+      if (space.innerHTML == '') {
+        if(goatleft>0 && !tigerTurn){
+            space.innerHTML='🐐';
+            flatboard[parseInt(space.id)].piece = piece("goat","🐐");
+            switchTurn();
+            decreGoatleft();
+        }
+        return; 
     }
-    space.style.border='2px solid black';
+
+   
+
+
+  if(firstclick){
+
+    space.style.border = '10px solid red';
+    highlightMoves(parseInt(space.id));
+    sourceSpace = space;
+    firstclick = false;
     
 }
+
+    else {
+        if(sourceSpace.id==space.id){
+            firstclick = true;
+            return;
+        }
+        makeTigerMove(parseInt(sourceSpace.id),parseInt(space.id));
+        firstclick=true;
+    }
+    
+    
+}
+
+function highlightMoves(indd){
+    const row = Math.floor(indd/5);
+    const col = indd % 5;
+
+    const targets = possibleSpaces(row,col);
+    console.log(targets);
+    for(const i of targets){
+        const flatIndex = i.row * 5 + i.col;
+        
+        const MySpace = document.getElementById(flatIndex);
+        MySpace.style.background="green";
+
+    }
+    
+
+    
+}
+
+export function emptySpace(spaceid){
+    document.getElementById(spaceid).innerHTML='';
+
+}
+export function insertPiece(spaceid,emoji){
+    document.getElementById(spaceid).innerHTML=emoji;
+
+}
+
 
